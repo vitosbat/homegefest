f499cd355f86b7a6ac53948b0cfd8568eab12813
Viktor <vitosbat@gmail.com>
2014-04-23 16:21:00 +0700

    Admin App: added editHousePassport page and front-side functions

--- public/admin/js/controllers/adminHousePassportCtrl.js
+++ public/admin/js/controllers/adminHousePassportCtrl.js
@@ -2,10 +2,17 @@
 
 angular
   .module('hgAdminApp')
-  .controller('adminHousePassportCtrl',['$scope', '$stateParams', 'Houses', function ($scope, $stateParams, Houses) {
+  .controller('adminHousePassportCtrl',['$scope', '$stateParams', '$location','Houses', function ($scope, $stateParams, $location, Houses) {
+    $scope.isEdit = false;
 
     var getHousePassport = function () {
       $scope.house = Houses.get({id: $stateParams.id});
+    }
+
+    var postHousePassport = function () {
+      Houses.save($scope.house, function () {
+        $location.path('/houses/:id');
+      });
     }
 
     getHousePassport();
